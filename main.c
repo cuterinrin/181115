330de7a30bfcc5b7fd5ca81e3409082e7ab2a04d
cuterinrin <yro7045@naver.com>
2018-11-15 16:16:55 +0900

    181115-ch11-4th

--- main.c
+++ main.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-	char *proverb="All that glisters is not gold.";
-	void setPointer(char **q)
-	{	
-	*q = proverb;
+	int sum(int a, int b)
+	{return a+b;
+	}
+	int sub(int a, int b)
+	{
+	return a-b;
+	}
+	int mul(int a, int b){
+	return a*b;
+	}
+	int div1(int a, int b)
+	{	return a/b;
 	}
 	
 int main(int argc, char *argv[]) {
 	
-	char *p = "zzz";
-	setPointer(&p);
-	printf("%s \n", p); 
+	
+	int(*calft)(int, int);
+	int a,b;
+	char op;
+	
+
+	printf("input calculation : ");
+	scanf("%d %c %d", &a, &op, &b);
+	
+	switch(op)
+	{
+		case '+':
+			calft=sum;
+			break;
+		
+		case '-':
+			calft=sub;
+			break;
+			
+		case '*':
+			calft=mul;
+			break;
+		
+		case '/':
+			calft=div1;
+			break;
+	}
+	
+	printf("°á°ú°ª %i", calft(a,b));
 	
 	return 0;
 }
